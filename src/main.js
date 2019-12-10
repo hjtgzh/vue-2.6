01bf00037391e822a58380fef5ef3855b1a107f3
hjtgzh <1246260533@qq.com>
2019-12-10 16:47:08 +0800

    添加element组件按需加载

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/index";
-// import "./uiConfig";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+import "../config/uiConfig";
 import "./assets/font/iconfont.css";
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
 
 new Vue({
   router,
